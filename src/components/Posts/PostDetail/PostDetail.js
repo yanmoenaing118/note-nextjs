57727e24e1d7e26546ee4e06d4bd34357bdb86d1
yanmoenaing118 <yan.webdev.1234@gmail.com>
2020-11-20 11:56:59 +0630

    mobile res

--- src/components/Posts/PostDetail/PostDetail.js
+++ src/components/Posts/PostDetail/PostDetail.js
@@ -3,7 +3,9 @@ import { MdSupervisorAccount } from "react-icons/md";
 import classes from "./PostDetail.module.scss";
 
 const PostDetail = (props) => {
-  const paras = props.post.body.split("\n").map((str) => <p>{str}</p>);
+  const paras = props.post.body
+    .split("\n")
+    .map((str, i) => <p key={i}>{str}</p>);
   return (
     <div className={classes.PostDetail}>
       <div>
